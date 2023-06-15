d09403171141486adff06400e6dc170e6474be1f
khalteck <khalidoyeneye@gmail.com>
2023-06-15 21:31:46 +0100

    aos

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -8,33 +8,38 @@ import execData from "../data/exec.json";
 import projectData from "../data/project.json";
 import niteData from "../data/quiznite.json";
 import fundraiserData from "../data/fundraiser.json";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Gallery = () => {
   const { loader } = useAppContext();
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
     <>
       <Header />
 
       {loader && <Loader />}
-      {/* <section className="w-full h-screen bg-white flex justify-center items-center">
-        <div className="flex flex-col gap-5">
-          <h2 className="uppercase font-medium text-[1.5rem]">
-            Under Construction
-          </h2>
-          <RingLoader color="#4b2a05" className="mx-auto" />
-        </div>
-      </section> */}
+
       <section className="w-full min-h-[200px] md:min-h-[300px] bg-white text-slate-700 pt-16 md:pt-[90px] bg-sec bg-no-repeat bg-cover md:bg-[center_top_-3rem] lg:bg-[center_top_-14rem]">
         <div className="w-full min-h-[200px]  md:min-h-[300px] lg:px-[15%] px-5 bg-black/50 flex justify-center items-center first-section-text text-[1.75rem] md:text-[2.5rem] text-white uppercase">
           <h1>STCOGA Gallery</h1>
         </div>
       </section>
-      <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 ">
+
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 "
+      >
         <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
           Executives
         </h1>
-        <p className="text-[.85rem]">Scroll to the left to see more...</p>
+        <p className="text-[.85rem]">Swipe left to see more...</p>
         <div className="w-full h-[400px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
           <div className="flex gap-4 snap-mandatory snap-x ">
             {execData?.map((item, index) => (
@@ -61,7 +66,11 @@ const Gallery = () => {
         </div>
       </section>
 
-      <section className="w-full flex md:flex-row flex-col gap-5 lg:px-[15%] px-5">
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full flex md:flex-row flex-col gap-5 lg:px-[15%] px-5"
+      >
         <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700">
           <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
             STCOGA UK & EUROPE - INCOMING EXCO
@@ -91,7 +100,11 @@ const Gallery = () => {
         </section>
       </section>
 
-      <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 ">
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 "
+      >
         <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
           Senior Secondary School Solar Power and Electricity Installation
           Project
@@ -99,7 +112,7 @@ const Gallery = () => {
         <h3 className="w-full font-bold text-[1.25rem] mb-6 text-end text-[#4b2a05]">
           February - March 2023
         </h3>
-        <p className="text-[.85rem]">Scroll to the left to see more...</p>
+        <p className="text-[.85rem]">Swipe left to see more...</p>
         <div className="w-full h-[400px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
           <div className="flex gap-4 snap-mandatory snap-x ">
             {projectData?.map((item, index) => (
@@ -133,11 +146,15 @@ const Gallery = () => {
         </div>{" "}
       </section>
 
-      <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 ">
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 "
+      >
         <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
           Trip to Ardfoyle Convent, Cork Ireland
         </h1>
-        <p className="text-[.85rem]">Scroll to the left to see more...</p>
+        <p className="text-[.85rem]">Swipe left to see more...</p>
 
         <div className="w-full h-[400px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
           <div className="flex gap-4 snap-mandatory snap-x ">
@@ -168,11 +185,15 @@ const Gallery = () => {
         </div>
       </section>
 
-      <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 ">
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 "
+      >
         <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
           NSF Quiz nite
         </h1>
-        <p className="text-[.85rem]">Scroll to the left to see more...</p>
+        <p className="text-[.85rem]">Swipe left to see more...</p>
         <div className="w-full h-[400px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
           <div className="flex gap-4 snap-mandatory snap-x ">
             {niteData?.map((item, index) => (
@@ -191,12 +212,16 @@ const Gallery = () => {
         </div>{" "}
       </section>
 
-      <section className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 ">
+      <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="w-full min-h-[400px] py-10 mb-5 text-slate-700 lg:px-[15%] px-5 "
+      >
         <h1 className="text-[1.2rem] md:text-[1.75rem] font-medium uppercase text-center mb-5 text-[#4b2a05]">
           St Teresa’s College Ibadan 90th Anniversary and Fundraising{" "}
         </h1>
-        <p className="text-[.85rem]">Scroll to the left to see more...</p>
-        <div className="w-full h-[400px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
+        <p className="text-[.85rem]">Swipe left to see more...</p>
+        <div className="w-full h-[470px] border-2 border-[#4b2a05]/50 flex gap-4 overflow-x-auto p-4 rounded-lg box">
           <div className="flex gap-4 snap-mandatory snap-x ">
             {fundraiserData?.map((item, index) => (
               <div
