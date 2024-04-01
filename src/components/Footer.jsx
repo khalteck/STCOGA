11fd604b6cf6444ed182f772b2befbafb868436f
khalteck <khalidoyeneye@gmail.com>
2024-04-01 17:24:05 +0100

    feat: VP image replace

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="w-full bg-[#262727] pt-[50px] text-[1rem] text-slate-300">
       <div className="w-full mb-[60px] px-4 sm:px-[30px] lg:px-[80px] flex flex-col md:flex-row gap-4 md:gap-8 justify-between"></div>
       <div className="w-full h-[80px] flex justify-center items-center text-[0.8rem] border-t border-slate-700 text-white">
-        Copyright © 2023, STCOGA UK & EUROPE
+        Copyright © 2024, STCOGA UK & EUROPE
       </div>
     </footer>
   );
